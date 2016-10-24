797706a89d1c6507e7b7d8ace9c1fe4e7a78eb36
gchevrot <guillaume.chevrot@gmail.com>
2016-10-24 15:52:08 +0200

    extract C alpha

--- pdbpy/coordinates.py
+++ pdbpy/coordinates.py
@@ -35,3 +35,38 @@ def extract_coordinates(pdb_name, download_from_pdb=True):
             if line[:4] == 'ATOM':
                 output.write(line)
 
+def extract_calpha_coordinates(pdb_name, download_from_pdb=True):
+    """
+    Extracting the lines containing the carbon alpha coordinates of the 1st chain
+
+    Parameters
+    ----------
+    pdb_name:
+        Name of the pdb file. (ex: 1dpx or 1dpx.pdb) 
+
+    download_from_pdb:
+        default is True. Use the download_pdb function (need an internet connection)
+        If False, use a local pdb file.
+
+    Return
+    ------
+    A text file (note that the coordinates are in Angstrom)
+    """
+    if download_from_pdb:
+        download_pdb(pdb_name)
+    if pdb_name[-4:] == '.pdb':
+        pdb_file = pdb_name
+        output_name = pdb_name[:-4]
+    else:
+        pdb_file = pdb_name + '.pdb'
+        output_name = pdb_name
+
+    with open(pdb_file, 'r') as input, open (output_name+'_calpha.pdb', 'w') as output:
+        for line in input:
+            # Save only the 1st chain
+            if line[:3] == 'TER':
+                break
+            if line[:4] == 'ATOM':
+                if line[13:15] == 'CA':
+                    output.write(line)
+
