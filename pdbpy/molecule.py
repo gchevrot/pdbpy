b9297bea759852f7209638b569269da43add7809
gchevrot <guillaume.chevrot@gmail.com>
2016-11-29 17:26:39 +0100

    msl for screwframe centers

--- pdbpy/molecule.py
+++ pdbpy/molecule.py
@@ -6,6 +6,7 @@ from pdbpy.data import aa_sidechain_chemical_properties as aa_hydrophobicity
 from pdbpy.msd import msd, msd_fft
 from pdbpy.download import download_pdb
 from pdbpy.inspection import is_dna_or_rna
+from pdbpy.screwframe import screwframe_rotation_centers
 
 
 class Molecule:
@@ -55,6 +56,18 @@ class Molecule:
 #        calpha_coordinates = extract_calpha_coordinates(self.pdb_name, download_from_pdb=self.download_from_pdb) 
 #        return calpha_coordinates
 
+    def screw_centers(self):
+        """
+        Compute the coordinates of the screwframe rotation centers from the C-alpha coordinates.
+
+        Return
+        ------
+        screwframe_centers: np.ndarray
+                    dimension: (nb of C-alpha atoms - 3, 3)
+        """
+        self.screwframe_centers = screwframe_rotation_centers(self.calpha_coordinates)
+        return self
+
     def number_of_residues(self):
         """
         Return
@@ -66,7 +79,7 @@ class Molecule:
             pdb_file = self.pdb_name
         else:
             pdb_file = self.pdb_name + '.pdb'
-        
+ 
         res_number = []
         with open(pdb_file, 'r') as f:
             for line in f:
@@ -131,8 +144,9 @@ class Molecule:
         if all_atoms:
             return msd(self.coordinates)
 
-    def msl_fft(self, calpha=True, all_atoms=False):
+    def msl_fft_old(self, calpha=True, all_atoms=False, screwframe_centers=False):
         """
+        This function is replaced by the static method msl_fft.
         Return the mean square length of the protein calculted with the C-alpha 
         atoms if calpha is True or for all atoms if all_atoms is True
         It is calculated like a MSD (using FFT in this case to calculate the MSD).
@@ -140,8 +154,39 @@ class Molecule:
         # if both are True, there is a problem, you should choose only one 
         if calpha and all_atoms:
             print("Choose calpha or all_atoms! You cannot choose both!")
+            sys.exit()
+        if calpha and screwframe_centers:
+            print("Choose calpha or screwframe_centers! You cannot choose both!")
+            sys.exit()
+        if screwframe_centers and all_atoms:
+            print("Choose screwframe_centers or all_atoms! You cannot choose both!")
+            sys.exit()
+        if calpha and all_atoms and screwframe_centers:
+            print("Choose calpha or all_atoms or screwframe_centers! You cannot choose all of them!")
+            sys.exit()
         if calpha:
             return msd_fft(self.calpha_coordinates)
         if all_atoms:
             return msd_fft(self.coordinates)
+        if screwframe_centers:
+            return msd_fft(self.screwframe_centers)
+
+    @staticmethod
+    def msl_fft(coordinates):
+        """
+        Compute the mean square length of the protein calculted with the C-alpha 
+        atoms if calpha is True or for all atoms if all_atoms is True
+        It is calculated like a MSD (using FFT in this case to calculate the MSD).
+ 
+        Parameters
+        ----------
+        coordinates: np.ndarray
+            dimension: (n, 3)
+
+        Return
+        ------
+        np.ndarray
+        dimension: (n,3)
+        """
+        return msd_fft(coordinates)
 
--- pdbpy/version.py
+++ pdbpy/version.py
@@ -1,1 +1,5 @@
-version = '0.4.4'
+# Notation x.y.z:
+# x: major release
+# y: minor release / new feature
+# z: build number / bug maintenance
+version = '0.5.1'
